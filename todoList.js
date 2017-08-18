566c3acf71c05f94a32e4f86f27a830ce8cf2e4a
Ivica Markovic <vitz01@gmail.com>
2017-08-18 16:30:25 +1000

    Update todoList.js

--- todoList.js
+++ todoList.js
@@ -252,7 +252,7 @@ fetch("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[post
 		}
 		response.json().then(function(data) {
 			var notepad = document.getElementById('notepad');
-			notepad.value = JSON.stringify(data);
+			notepad.value = data.0.content;
 			console.log(data);
 		});
 }).catch(function(err) {
